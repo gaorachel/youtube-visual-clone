06b5b06d1b808fd4fefd78c124305124be39db51
Rachel <gggggrea@gmail.com>
2023-04-17 16:48:43 +0100

    Add two more sections on sidebar

--- src/pages/HomePage/SideBar/SideBar.jsx
+++ src/pages/HomePage/SideBar/SideBar.jsx
@@ -8,6 +8,15 @@ import { ReactComponent as YourVideoIcon } from "./svg/yourVideos.svg";
 import { ReactComponent as WatchLaterIcon } from "./svg/watchLater.svg";
 import { ReactComponent as PlayListIcon } from "./svg/playList.svg";
 import { ReactComponent as LikedVideoIcon } from "./svg/likedVideos.svg";
+import { ReactComponent as TrendingIcon } from "./svg/trending.svg";
+import { ReactComponent as MusicIcon } from "./svg/music.svg";
+import { ReactComponent as MoviesAndShowsIcon } from "./svg/moviesAndShows.svg";
+import { ReactComponent as LiveIcon } from "./svg/live.svg";
+import { ReactComponent as GamingIcon } from "./svg/gaming.svg";
+import { ReactComponent as NewsIcon } from "./svg/news.svg";
+import { ReactComponent as SportIcon } from "./svg/sport.svg";
+import { ReactComponent as LearningIcon } from "./svg/learning.svg";
+import { ReactComponent as FashionAndBeautyIcon } from "./svg/fashionAndBeauty.svg";
 
 import classNames from "classnames";
 
@@ -87,6 +96,67 @@ export function SideBar() {
           <LikedVideoIcon className={styles.navIcon} style={iconStyle} />
           <a href={link} className={styles.navText}>
             Liked Videos
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.navSection}>
+        <div className={styles.sectionHeader}>Subscriptions</div>
+      </div>
+      <div className={styles.navSection}>
+        <div className={styles.sectionHeader}>Explore</div>
+        <div className={styles.navBox}>
+          <TrendingIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            Trending
+          </a>
+        </div>
+        <div className={styles.navBox}>
+          <MusicIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            Music
+          </a>
+        </div>
+        <div className={styles.navBox}>
+          <MoviesAndShowsIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            Movies & shows
+          </a>
+        </div>
+        <div className={styles.navBox}>
+          <LiveIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            Live
+          </a>
+        </div>
+        <div className={styles.navBox}>
+          <GamingIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            Gaming
+          </a>
+        </div>
+        <div className={styles.navBox}>
+          <NewsIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            News
+          </a>
+        </div>
+        <div className={styles.navBox}>
+          <SportIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            Sport
+          </a>
+        </div>
+        <div className={styles.navBox}>
+          <LearningIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            Learning
+          </a>
+        </div>
+        <div className={styles.navBox}>
+          <FashionAndBeautyIcon className={styles.navIcon} style={iconStyle} />
+          <a href={link} className={styles.navText}>
+            Fashion & Beauty
           </a>
         </div>
       </div>
