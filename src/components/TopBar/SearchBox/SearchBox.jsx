ba3de272db71071c938211a60465a7e0048eb2eb
Rachel <gggggrea@gmail.com>
2023-04-18 17:49:34 +0100

    Use className instead of style

--- src/components/TopBar/SearchBox/SearchBox.jsx
+++ src/components/TopBar/SearchBox/SearchBox.jsx
@@ -4,9 +4,9 @@ import { ReactComponent as SearchIcon } from "../svg/searchIcon.svg";
 export function SearchBox() {
   return (
     <form className={styles.searchBox}>
-      <input className={styles.searchBar} placeholder="Search" style={{ color: "#FFFFFF" }} />
+      <input className={styles.searchBar} placeholder="Search" />
       <button className={styles.searchBarButton}>
-        <SearchIcon style={{ fill: "#FFFFFF", width: 24, height: 24 }} />
+        <SearchIcon className={styles.searchIcon} />
       </button>
     </form>
   );
