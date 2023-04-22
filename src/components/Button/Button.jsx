6806549562418b5b6637b6047aad7635ad6ccfc7
Rachel <gggggrea@gmail.com>
2023-04-22 19:00:46 +0100

    Create a  full-rounded button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./Button.module.css";
 
-export function Button() {
-  return <button className="">button</button>;
+export function FullRoundedButton({ onClick, className, buttonText }) {
+  return (
+    <button onClick={onClick} className={classNames(styles.fullRoundedButton, className)}>
+      {buttonText}
+    </button>
+  );
 }
