3c5509b0a4513762b7fe802df17084655e06597b
Rachel <gggggrea@gmail.com>
2023-04-23 22:19:37 +0100

    Change chip bar to button with cursor pointer

--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -2,5 +2,5 @@ import styles from "./Chip.module.css";
 import classNames from "classnames";
 
 export function Chip({ className, chipText }) {
-  return <div className={classNames(styles.chip, className)}>{chipText}</div>;
+  return <button className={classNames(styles.chip, className)}>{chipText}</button>;
 }
