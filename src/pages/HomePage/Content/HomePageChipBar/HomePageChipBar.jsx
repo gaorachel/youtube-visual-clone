d588fc502398b929a5f20ee1ebc095f33a52f140
Rachel <gggggrea@gmail.com>
2023-04-23 14:31:26 +0100

    Add dynamic click and scroll features on chip bar

--- src/pages/HomePage/Content/HomePageChipBar/HomePageChipBar.jsx
+++ src/pages/HomePage/Content/HomePageChipBar/HomePageChipBar.jsx
@@ -1,11 +1,13 @@
 import { Chip } from "components/Chip/Chip";
 import { FullRoundedButton } from "components/Button/Button";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./HomePageChipBar.module.css";
 
 export function FeedFilterChipBar() {
   const ref = useRef(null);
+  const [scrollX, setScrollX] = useState(0);
+  const moveDistance = 200;
 
   const chipArr = [
     "All",
@@ -33,24 +35,37 @@ export function FeedFilterChipBar() {
     "Vestibulum",
     "Phasellus aliquam",
   ];
+
   return (
     <div className={styles.chipBarWithButton}>
       <div>
         <FullRoundedButton
-          onClick={() => (ref.current.scrollLeft -= 200)}
-          className={styles.chipBarButton}
+          onClick={() => {
+            setScrollX(scrollX - moveDistance);
+            ref.current.scrollLeft -= moveDistance;
+          }}
+          className={scrollX > 0 ? styles.chipBarButton : styles.chipBarButtonHidden}
           buttonText={<IoIosArrowBack size="16px" />}
         />
       </div>
-      <div className={styles.chipBar} ref={ref}>
+      <div
+        className={styles.chipBar}
+        ref={ref}
+        onScroll={(el) => {
+          setScrollX(el.target.scrollLeft);
+        }}
+      >
         {chipArr.map((chip) => {
           return <Chip className={styles.homePageChip} chipText={chip} key={chip} />;
         })}
       </div>
       <div>
         <FullRoundedButton
-          onClick={() => (ref.current.scrollLeft += 200)}
-          className={styles.chipBarButton}
+          onClick={() => {
+            setScrollX(scrollX + moveDistance);
+            ref.current.scrollLeft += moveDistance;
+          }}
+          className={scrollX < ref.current?.clientWidth ?? 0 - 50 ? styles.chipBarButton : styles.chipBarButtonHidden}
           buttonText={<IoIosArrowForward size="16px" />}
         />
       </div>
